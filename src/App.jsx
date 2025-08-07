c228734a6852a695a90887237a8ca3c0abf661fe
ncardona90 <132845976+ncardona90@users.noreply.github.com>
2025-08-07 15:16:56 -0500

    chore: initialize React project with Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Formulario de Tamizaje</h1>
+      <p>Aplicación React en progreso.</p>
+    </div>
+  )
+}
+
+export default App
